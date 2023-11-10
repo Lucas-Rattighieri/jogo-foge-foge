65de8a9894a518fb50fccfe711ae75446a45a9c8
Lucas Rattighieri <lucas.rattighieri@gmail.com>
2023-11-10 18:27:15 -0300

    Alocação Dinâmica do mapa

--- Foge_foge.c
+++ Foge_foge.c
@@ -1,8 +1,11 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+char ** mapa;
+int linhas, colunas;
+
 int main() {
-    char mapa[5][10+1];
+    
 
     FILE *f;
 
@@ -12,9 +15,23 @@ int main() {
         printf("Erro na leitura do mapa\n");
         exit(1);
     }
+    
+    fscanf(f, "%d %d", &linhas, &colunas);
+    
+    mapa = (char **) malloc(sizeof(char *) * linhas);
+
+    for (int i = 0; i < linhas; i++) {
+        mapa[i] = (char *) malloc(sizeof(char) * (colunas + 1));
+    }
+
     for (int i = 0; i < 5; i++)
         fscanf(f, "%s", mapa[i]);
 
     for (int i = 0; i < 5; i++)
         printf("%s\n", mapa[i]);
+
+    for (int i = 0; i < linhas; i++) {
+        free(mapa[i]);
+    }
+    free(mapa);
 }
