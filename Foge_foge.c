1d597e5c1910bb9ba5dd942247846c4002048f14
Lucas Rattighieri <lucas.rattighieri@gmail.com>
2023-11-16 17:59:11 -0300

    Acrescentando os Fantasmas

--- Foge_foge.c
+++ Foge_foge.c
@@ -1,22 +1,32 @@
 #include "mapa.h"
 
 Mapa mapa_jogo;
+Heroi heroi_jogo;
+Fantasmas fantasmas_jogo;
 
 
 int main() {
 
     char comando;
+
+    iniciar_heroi(&heroi_jogo, HEROI, 0, 0);
+
     
-    ler_mapa(&mapa_jogo);
+    ler_mapa(&mapa_jogo, &heroi_jogo, &fantasmas_jogo);
 
     do {
         imprimir_mapa(&mapa_jogo);
 
         comando = getche();
 
-        mover(&mapa_jogo, comando);
+        mover_heroi(&mapa_jogo, &heroi_jogo, comando);
+        mover_fantasmas(&mapa_jogo, &heroi_jogo, &fantasmas_jogo);
 
-    } while (!jogo_acabou());
+    } while (!jogo_acabou(&mapa_jogo, &heroi_jogo));
+
+    imprimir_mapa(&mapa_jogo);
+    printf("\nQue pena, voce perdeu!\n\n");
 
     liberar_mapa(&mapa_jogo);
+    liberar_fantasmas(&fantasmas_jogo);
 }
--- mapa.h
+++ mapa.h
@@ -1,26 +1,65 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <conio.h>
+# include <time.h>
+# include <math.h>
+# include "ui.h"
+
+# define HEROI '@'
+# define FANTASMA 'F'
+# define BOMBA 'P'
+# define PAREDE_VERTICAL '|'
+# define PAREDE_HORIZONTAL '-'
+
+
+typedef struct {
+    int x;
+    int y;
+} Posicao;
 
 
 typedef struct {
     char ** matriz;
     int linhas;
     int colunas;
-    int x;
-    int y;
 } Mapa;
 
 
+typedef struct {
+    char aparencia;
+    Posicao posicao;
+} Heroi;
 
-// Funções
-int mod(int num, int N) {
-    if (num >= 0) 
-        return num % N;
-    else {
-        return N + (num % N);
-    }
+
+typedef struct {
+    int num;
+    char aparencia;
+    Posicao * posicoes;
+} Fantasmas;
+
+
+
+
+void iniciar_heroi(Heroi * heroi, char aparencia, int x, int y) {
+    heroi->aparencia = aparencia;
+    heroi->posicao.x = x;
+    heroi->posicao.y = y;
 }
+
+
+void iniciar_fantasmas(Fantasmas * fantasmas, int num_fantasmas, char aparencia) {
+    fantasmas->num = num_fantasmas;
+    fantasmas->aparencia = aparencia;
+
+    fantasmas->posicoes = (Posicao *) malloc(sizeof(Posicao) * num_fantasmas);
+}
+
+
+void liberar_fantasmas(Fantasmas * fantasmas) {
+
+    free(fantasmas->posicoes);
+}
+
 
 
 void declara_mapa(Mapa * mapa) {
@@ -32,21 +71,44 @@ void declara_mapa(Mapa * mapa) {
 }
 
 
-void encontrar_posicao(Mapa * mapa) {
+
+
+int encontrar_heroi(Mapa * mapa, Heroi * heroi) {
     for (int i = 0; i < mapa->linhas; i++) {
         for (int j = 0; j < mapa->colunas; j++) {
-            if (mapa->matriz[i][j] == '@') {
-                mapa->x = j;
-                mapa->y = i;
-                break;
+
+            if (mapa->matriz[i][j] == heroi->aparencia) {
+                heroi->posicao.x = j;
+                heroi->posicao.y = i;
+                return 1;
             }
+        }
+    }
+    return 0;
+}
+
+
+void encontrar_fantasmas(Mapa * mapa, Fantasmas * fantasmas, Heroi * heroi) {
+    int k = 0;
+    for (int i = 0; i < mapa->linhas; i++) {
+        for (int j = 0; j < mapa->colunas; j++) {
+
+            if (mapa->matriz[i][j] == fantasmas->aparencia) {
+                fantasmas->posicoes[k].x = j;
+                fantasmas->posicoes[k].y = i;
+                k++;
+            }
+
+            if (k == fantasmas->num)
+                break;
         }
     }
 }
 
 
-void ler_mapa(Mapa * mapa) {
+void ler_mapa(Mapa * mapa, Heroi * heroi, Fantasmas * fantasmas) {
     FILE *f;
+    int num_fantasmas;
 
     f = fopen("mapa.txt", "r");
 
@@ -55,16 +117,19 @@ void ler_mapa(Mapa * mapa) {
         exit(1);
     }
     
-    fscanf(f, "%d %d", &(mapa->linhas), &(mapa->colunas));
+    fscanf(f, "%d %d %d", &(mapa->linhas), &(mapa->colunas), &num_fantasmas);
     
     declara_mapa(mapa);
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < mapa->linhas; i++)
         fscanf(f, "%s", mapa->matriz[i]);
     
     fclose(f);
 
-    encontrar_posicao(mapa);
+    iniciar_fantasmas(fantasmas, num_fantasmas, FANTASMA);
+
+    encontrar_heroi(mapa, heroi);
+    encontrar_fantasmas(mapa, fantasmas, heroi);
 }
 
 
@@ -76,47 +141,156 @@ void liberar_mapa(Mapa * mapa) {
 }
 
 
+void imprimeparte(char desenho[4][7], int parte) {
+    printf("%s", desenho[parte]);
+}
+
+
 void imprimir_mapa(Mapa * mapa) {
-    printf("\n");
+    for(int i = 0; i < mapa->linhas; i++) {
 
-    for (int i = 0; i < mapa->linhas; i++) {
-        printf("%s\n", mapa->matriz[i]);
+        for(int parte = 0; parte < 4; parte++) {
+            for(int j = 0; j < mapa->colunas; j++) {
+
+                switch(mapa->matriz[i][j]) {
+                    case FANTASMA:
+                        imprimeparte(desenhofantasma, parte);
+                        break;
+                    case HEROI:
+                        imprimeparte(desenhoheroi, parte);
+                        break;
+                    case BOMBA:
+                        imprimeparte(desenhopilula, parte);
+                        break;
+                    case PAREDE_HORIZONTAL:
+                    case PAREDE_VERTICAL:
+                        imprimeparte(desenhoparede, parte);
+                        break;
+                    case '.':
+                        imprimeparte(desenhovazio, parte);
+                        break;
+                }
+
+            }
+            printf("\n");
+        }
+
     }
 }
 
 
-int mover(Mapa * mapa, char direcao) {
-    
-    mapa->matriz[mapa->y][mapa->x] = '.';
-    switch (direcao) {
-        case 'a':
-            mapa->matriz[mapa->y][mod(mapa->x - 1, mapa->colunas)] = '@';
-            mapa->x = mod(mapa->x - 1, mapa->colunas);
-            break;
-        case 'w':
-            mapa->matriz[mod(mapa->y-1, mapa->linhas)][mapa->x] = '@';
-            mapa->y = mod(mapa->y-1, mapa->linhas);
-            break;
-        case 'd':
-            mapa->matriz[mapa->y][mod(mapa->x+1, mapa->colunas)] = '@';
-            mapa->x = mod(mapa->x+1, mapa->colunas);
-            break;
-        case 's':
-            mapa->matriz[mod(mapa->y+1, mapa->linhas)][mapa->x] = '@';
-            mapa->y = mod(mapa->y+1, mapa->linhas);
-            break;
-        default:
-            mapa->matriz[mapa->y][mapa->x] = '@';
+int posicao_vazio(Mapa * mapa, int x, int y) {
+    if (mapa->matriz[y][x] != '.')
+        return 0;
+    else 
+        return 1;
+}
 
-            if (direcao == 27)
-                exit(0);
+
+int posicao_fantasma(Mapa * mapa, Fantasmas * fantasmas, int x, int y) {
+    if (mapa->matriz[y][x] != fantasmas->aparencia)
+        return 0;
+    else 
+        return 1;
+}
+
+
+int posicao_heroi(Mapa * mapa, Heroi * heroi, int x, int y) {
+    if (mapa->matriz[y][x] != heroi->aparencia)
+        return 0;
+    else 
+        return 1;
+}
+
+
+int mover_heroi(Mapa * mapa, Heroi * heroi, char tecla) {
+    
+    int prox_x = heroi->posicao.x, prox_y = heroi->posicao.y;
+
+	if(tecla == 'w' || tecla == 'W' || tecla == 72){
+        prox_y--;
+    }                    
+    if(tecla == 'a' || tecla == 'A' || tecla == 75){
+        prox_x--;
+    }                    
+    if(tecla == 's' || tecla == 'S' || tecla == 80){
+        prox_y++;
+    }                     
+    if(tecla == 'd' || tecla == 'D' || tecla == 77){
+        prox_x++;
+    } 
+
+
+    if (posicao_vazio(mapa, prox_x, prox_y)) {
+        mapa->matriz[heroi->posicao.y][heroi->posicao.x] = '.';
+        mapa->matriz[prox_y][prox_x] = heroi->aparencia;
+        heroi->posicao.x = prox_x;
+        heroi->posicao.y = prox_y;
     }
     
 }
 
 
-int jogo_acabou() {
+float distancia(Posicao posicao1, Posicao posicao2) {
+    float dist; 
+
+    return (float) sqrt(pow(posicao1.x - posicao2.x, 2) + pow(posicao1.y - posicao2.y, 2));
+}
 
 
-    return 0;
+int mover_fantasmas(Mapa * mapa, Heroi * heroi, Fantasmas * fantasmas) {
+    
+    int candidato_x, candidato_y, prox_x, prox_y;
+    int pos_x, pos_y, mover;
+    Posicao movimetacoes[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    float distancia_atual, nova_distancia;
+    
+
+    srand(time(NULL));
+
+    for (int i = 0; i < fantasmas->num; i++) {
+        pos_x = fantasmas->posicoes[i].x;
+        pos_y = fantasmas->posicoes[i].y;
+
+        prox_x = pos_x;
+        prox_y = pos_y;
+
+        distancia_atual = distancia(heroi->posicao, fantasmas->posicoes[i]);
+
+        for (int j = 0; j < 4; j++)  {
+            candidato_x = pos_x + movimetacoes[j].x;
+            candidato_y =  pos_y + movimetacoes[j].y;
+
+            Posicao nova_posicao = {candidato_x, candidato_y};
+
+            nova_distancia = distancia(heroi->posicao, nova_posicao);
+            
+            if (posicao_vazio(mapa, candidato_x, candidato_y) || 
+                posicao_heroi(mapa, heroi, candidato_x, candidato_y)) {
+
+                if (distancia_atual > nova_distancia) {
+                    prox_x = candidato_x;
+                    prox_y = candidato_y;
+                    distancia_atual = nova_distancia;
+                }
+                else if (prox_x == pos_x && prox_y == pos_y) {
+                    prox_x = candidato_x;
+                    prox_y = candidato_y;
+                }
+            }
+        } 
+                
+
+        mapa->matriz[pos_y][pos_x] = '.';
+        mapa->matriz[prox_y][prox_x] = fantasmas->aparencia;
+        fantasmas->posicoes[i].x = prox_x;
+        fantasmas->posicoes[i].y = prox_y;
+    }
+    
+}
+
+
+int jogo_acabou(Mapa * mapa, Heroi * heroi) {
+    
+    return !encontrar_heroi(mapa, heroi);
 }
